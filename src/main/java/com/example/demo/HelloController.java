da083d730aa0bc663d8e4236637e1c9349ff47fd
Slayniaccc <212771087+Slayniaccc@users.noreply.github.com>
2026-04-26 17:50:18 +0100

    Add GET /tasks endpoint

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    ArrayList<Task> tasks = new ArrayList<>();
+
     @GetMapping("/tasks")
-    public String test() {
-        return "API working";
+    public ArrayList<Task> getTasks() {
+        return tasks;
     }
 }
