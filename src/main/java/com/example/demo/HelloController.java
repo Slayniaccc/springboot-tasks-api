43b350f932b729cdb6607cbc23feb063d39c423f
Slayniaccc <212771087+Slayniaccc@users.noreply.github.com>
2026-04-26 17:51:46 +0100

    Add POST /tasks endpoint

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +16,9 @@ public class HelloController {
     public ArrayList<Task> getTasks() {
         return tasks;
     }
+    @PostMapping("/tasks")
+public Task postTask(@RequestBody Task task) {
+    tasks.add(task);
+    return task;
+}
 }
