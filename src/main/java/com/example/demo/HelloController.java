f2652e3bb89c64f2805581085a859164d74bf3d7
Slayniaccc <212771087+Slayniaccc@users.noreply.github.com>
2026-04-26 17:49:02 +0100

    Add basic controller with test endpoint

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/tasks")
+    public String test() {
+        return "API working";
+    }
+}
